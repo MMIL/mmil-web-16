afd78484b5d397c399cae95db8dbcc12d92b7ea5
Anant Singh <anantsingh1010@gmail.com>
2016-04-06 16:59:32 +0530

    Favicon link update

--- pages/zeal.php
+++ pages/zeal.php
@@ -15,7 +15,7 @@
 <!--Import Google Icon Font-->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-<link rel="shortcut icon" type="image/x-icon" href="icon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="../icon.ico">
 <!--this link is for the social icons-->
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <style type="text/css">
